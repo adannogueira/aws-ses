58ce754e7341802eea2cf7c6556a9cd71cc8a8f2
Adan Nogueira <adan.nogueira@gmail.com>
2023-01-28 16:25:24 -0300

    feat: handle missing data

--- handler.ts
+++ handler.ts
@@ -4,6 +4,9 @@ import { MailSender } from './MailSender';
 
 export const createContact = async ({ body }) => {
   const { from, to, subject, message } = JSON.parse(body);
+  if (!from || !to || !subject || !message) {
+    return { statusCode: 400, body: JSON.stringify({ message: 'Missing parameters' }) }
+  }
   const mailSender = new MailSender()
   const result = await mailSender
     .from(from)
